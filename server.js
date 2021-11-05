70c2873f008efb939d45b7483da100ccb5abd692
Ankit-Developer143 <dante7785@gmail.com>
2021-11-05 16:55:42 +0530

    Fastify | validation | Handler | controller

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const fastify = require("fastify")({ logger: true });
+fastify.register(require('fastify-swagger'),{
+    exposeRoute:true,
+    routePrefix:'/docs',
+    swagger:{
+        info:{title:'fastify-api'}
+    }
+})
+
+// for Swagger
+//http://127.0.0.1:5000/docs/static/index.html#/
+
 const PORT = 5000;
 //register fastify routes 
 //fastify.register(require('./routes/simpleRouting'))
