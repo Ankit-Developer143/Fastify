8389c7b3280b9ec21f0e59a2a19b786e6efe0701
Ankit-Developer143 <dante7785@gmail.com>
2021-11-05 14:57:41 +0530

    Fastify | Simple Validation

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const fastify = require("fastify")({ logger: true });
 const PORT = 5000;
 //register fastify routes 
-fastify.register(require('./routes/simpleRouting'))
+//fastify.register(require('./routes/simpleRouting'))
 //fastify.register(require('./routes/itemHandlerExample'));
-//fastify.register(require('./routes/itemSimpleValidation'));
+fastify.register(require('./routes/itemSimpleValidation'));
 
 
 const start = async () => {
